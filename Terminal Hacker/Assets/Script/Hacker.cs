a25bc6bc16c2f12c584119dfde806d928b2e43d5
MWIMAF <mwimaf@student.telkomuniversity.ac.id>
2022-05-28 00:34:31 +0700

    Main menu (Learning)

---
+++ Terminal Hacker/Assets/Script/Hacker.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowMainMenu("[NAME]");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ShowMainMenu(string greeting)
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Hello "+greeting);
+        Terminal.WriteLine("What would you like to hack into?");
+        Terminal.WriteLine("Press 1 to the local library");
+        Terminal.WriteLine("Press 2 to the police station");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("Input here: ");
+    }
+}
