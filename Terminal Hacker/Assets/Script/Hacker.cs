27190c29fd9e2afa554f49ffda8ea5a16c47e476
MWIMAF <mwimaf@student.telkomuniversity.ac.id>
2022-05-29 22:59:17 +0700

    Password (learning)

--- Terminal Hacker/Assets/Script/Hacker.cs
+++ Terminal Hacker/Assets/Script/Hacker.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class Hacker : MonoBehaviour
 {
+    int lvl;
+    string password;
+
+    enum Screen { MainMenu, Password, Win };
+    Screen currentScreen;
+
     // Start is called before the first frame update
     void Start()
     {
         ShowMainMenu("[NAME]");
+        currentScreen = Screen.MainMenu;
     }
 
     // Update is called once per frame
@@ -32,15 +39,59 @@ public class Hacker : MonoBehaviour
         if (input == "menu")
         {
             ShowMainMenu("[NAME]");
+            currentScreen = Screen.MainMenu;
         }
-        else if(input == "1")
+        else if (currentScreen == Screen.MainMenu)
         {
-            print("you chose "+ input);
+            RunMainMenu(input);
+        }
+        else if (currentScreen == Screen.Password)
+        {
+            CheckPassword(input);
+        }
+
+        void RunMainMenu(string input)
+        {
+            if (input == "1")
+            {
+                lvl = 1;
+                password = "0000";
+                StartGame();
+            }
+            else if (input == "2")
+            {
+                lvl = 2;
+                password = "2103";
+                StartGame();
+            }
+            else
+            {
+                Terminal.WriteLine("Please input valid number");
+            }
+        }
+
+    }
+
+    void StartGame ()
+    {
+        currentScreen = Screen.Password;
+        Terminal.ClearScreen();
+        Terminal.WriteLine("You have chosen level " + lvl);
+        Terminal.WriteLine("Please Enter password: ");
+    }
+
+    void CheckPassword (string input)
+    {
+        if (input == password)
+        {
+            Terminal.ClearScreen();
+            Terminal.WriteLine("Well done! You're in!");
+            Terminal.WriteLine("");
+            Terminal.WriteLine("Type 'menu' to back to main menu!");
         }
         else
         {
-            print("false");
+            Terminal.WriteLine("ups, that's wrong!");
         }
-
     }
 }
